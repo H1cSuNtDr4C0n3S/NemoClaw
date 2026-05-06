c19a09ea41318f5f2369f440095cd079a89e7723
Michael Spina <h1csuntdr4c0n3s@gmail.com>
2026-05-07 01:12:26 +0200

    "test-fix-openclaw-guard-pass-through-on-linux"

--- test/nemoclaw-start.test.ts
+++ test/nemoclaw-start.test.ts
@@ -509,7 +509,7 @@ describe("nemoclaw-start configure guard behavior", () => {
       "bash",
       [
         "--norc",
-        "-lc",
+        "-c",
         [
           `source ${JSON.stringify(setup.proxyEnv)}`,
           ["openclaw", ...args.map((arg) => JSON.stringify(arg))].join(" "),
